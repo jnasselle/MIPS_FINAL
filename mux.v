52319c76c7653a35aef6b547dcf197b8af8948c9
sureda jose <suredajose@gmail.com>
2015-03-02 14:27:08 -0300

    Agregue mux 4 entradas

---
+++ mux.v
@@ -0,0 +1,44 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    17:37:38 02/03/2015 
+// Design Name: 
+// Module Name:    mux
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module mux
+  #(
+	 parameter WIDTH = 32   //Ancho de la direccion de memoria.
+	 )
+	(
+    input  [WIDTH-1:0] in0,
+    input  [WIDTH-1:0] in1,
+    input  [WIDTH-1:0] in2,
+	input  [WIDTH-1:0] in3,
+    output reg [WIDTH-1:0] out,
+    input  [1:0] sel
+    );
+
+	always @(*)
+	begin
+		case (sel)
+			2'b00: 	out <= in0;
+			2'b01:	out <= in1;
+			2'b10:	out <= in2;
+			2'b10:	out	<= in3;
+			default: out <= 0;
+		endcase
+	end
+endmodule
