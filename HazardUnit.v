f822e1dfc05f09a311c017b375605a2b626aa1f9
sureda jose <suredajose@gmail.com>
2015-05-19 11:54:39 -0300

    Correcciones HazardUnit

--- HazardUnit.v
+++ HazardUnit.v
@@ -26,32 +26,32 @@ module HazardUnit(
 	input [5:0] WriteRegE,
 	input [5:0] WriteRegM,
 	input [5:0] WriteRegW,
+	input RegWriteE,
+	input RegWriteM,
+	input RegWriteW,
+	input MemToReg,
+	input BranchD,
 	output StallF,
 	output StallD,
-	output BranchD,
 	output ForwardAD,
 	output ForwardBD,
 	output FlushE,
 	output reg [1:0] ForwardAE,
 	output reg [1:0] ForwardBE,
-	output MemToReg,
-	output RegWriteE,
-	output RegWriteM,
-	output RegWriteW
    );
 	
 always@(*)
 begin
 	if ((RsE != 0) && (RsE == WriteRegM) && RegWriteM)
 	ForwardAE = 2'b10;
-	else if ((RsE !=0) && (RsE == WriteRegW) && RegWriteW)
+	else if ((RsE != 0) && (RsE == WriteRegW) && RegWriteW)
 	ForwardAE = 2'b01;
 	else
 	ForwardAE = 2'b00;
 	
 	if ((RtE != 0) && (RtE == WriteRegM) && RegWriteM)
 	ForwardBE = 2'b10;
-	else if ((RtE !=0) && (RtE == WriteRegW) && RegWriteW)
+	else if ((RtE != 0) && (RtE == WriteRegW) && RegWriteW)
 	ForwardBE = 2'b01;
 	else
 	ForwardBE = 2'b00;
