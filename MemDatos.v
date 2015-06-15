a0b7929c26c52d8290fc95011becf961b974284b
Juan Nicolas Asselle <jnasselle@gmail.com>
2015-06-15 11:24:35 -0300

    Se cambio el IP Core por el archivo  a mano

--- MemDatos.v
+++ MemDatos.v
@@ -19,7 +19,41 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module MemDatos(
+  wire clka, // input clka
+  input [2:0] write, // input [3 : 0] wea
+  input [2:0] read,
+  input [31:0] addr, // input [31 : 0] addra
+  input [31:0] dina, // input [31 : 0] dina
+  output reg [31:0] dout,
+  output reg [31:0] memoria[15:0]	//Para almacenar 16 palabra
     );
+localparam WORD = 0;
+localparam HALFWORD = 1;
+localparam BYTE = 2;
+
+// TODO: ver ENDIANESS de la memoria
+always@(posedge clka)
+begin
+	case (read)
+		WORD:
+			dout<=memoria[addr];
+		HALFWORD:
+			dout<=memoria[addr] & 32'hFFFF0000;
+		BYTE:
+			dout<=memoria[addr] & 32'hFF000000;
+	endcase
+end
 
 
+always@(posedge clka)
+begin
+	case (write)
+		WORD:
+			memoria[addr]<=din;
+		HALFWORD:
+			memoria[addr]<=din & 32'hFFFF0000;
+		BYTE:
+			memoria[addr]<=din & 32'hFF000000;	
+	endcase
+end
 endmodule
